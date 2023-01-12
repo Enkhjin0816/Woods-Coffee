3b303548761b061af0083cdaf4aa6a9d72a5b05e
EnkhjinHop-3 <enkhjin@iMac-17s-iMac.local>
2023-01-12 19:19:58 +0800

    eight commit

--- screens/Details.tsx
+++ screens/Details.tsx
@@ -1,12 +1,30 @@
-import { View, Text } from "react-native"
+import { Button, TextInput, View } from "react-native"
 import React from "react";
+import { Formik } from 'formik';
 
-const Details = () => {
-    return(
-        <View>
-            <Text>aa detail!</Text>
-        </View>
-    )
+const Details = (props: any) => {
+   return (
+    <Formik
+    initialValues={{ email: '', pass: '' }}
+    onSubmit={values => console.log(values)}
+  >
+    {({ handleChange, handleBlur, handleSubmit, values }) => (
+      <View>
+        <TextInput
+          onChangeText={handleChange('email')}
+          onBlur={handleBlur('email')}
+          value={values.email}
+        />
+        <TextInput
+          onChangeText={handleChange('pass')}
+          onBlur={handleBlur('pass')}
+          value={values.pass}
+        />
+        <Button onPress={handleSubmit} title="Submit" />
+      </View>
+    )}
+  </Formik>
+   );
 }
 
 export default Details;
